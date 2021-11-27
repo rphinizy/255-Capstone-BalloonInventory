bd1ccfb09a7be85fd37e5c13464669a64fd61e76
rphinizy <robinphinizy@yahoo.com>
2021-11-27 02:03:52 -0500

    Add project files.

---
+++ BalloonInventory/Models/InventoryItems.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BalloonInventory.Models
+{
+    public class InventoryItem
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Size { get; set; }
+
+        public string Color { get; set; }
+
+        public int QTY { get; set; }
+
+        public int Price { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
---
+++ BalloonInventory/Services/InventoryServices.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using MongoDB.Driver;
+using BalloonInventory.Models;
+
+namespace BalloonInventory.Services
+{
+    public class InventoryServices
+    {
+        private readonly IMongoCollection<InventoryItem> _inventoryItem;
+
+        public InventoryServices(IDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _inventoryItem = database.GetCollection<InventoryItem>("InventoryItems");
+        }
+
+        public InventoryItem Create(InventoryItem item)
+        {
+            _inventoryItem.InsertOne(item);
+            return item;
+        }
+
+        public IList<InventoryItem> Read() =>
+           _inventoryItem.Find(ite => true).ToList();
+
+        public InventoryItem Find(string id) =>
+            _inventoryItem.Find(ite => ite.Id == id).SingleOrDefault();
+
+        public void Update(InventoryItem item) =>
+          _inventoryItem.ReplaceOne(ite => ite.Id == item.Id, item);
+
+        public void Delete(string id) =>
+            _inventoryItem.DeleteOne(ite => ite.Id == id);
+    }
+}
